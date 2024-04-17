9d6ff70381a61241a26bbaa5a3cae58e375427d3
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-04-17 17:00:20 +0900

    lint and format

--- apps/demo/src/main-page.ts
+++ apps/demo/src/main-page.ts
@@ -1,7 +1,8 @@
-import { EventData, Page } from '@nativescript/core'
-import { HelloWorldModel } from './main-view-model'
+import type { EventData, Page } from "@nativescript/core";
+
+import { HelloWorldModel } from "./main-view-model";
 
 export function navigatingTo(args: EventData) {
-  const page = <Page>args.object
-  page.bindingContext = new HelloWorldModel()
+  const page = <Page>args.object;
+  page.bindingContext = new HelloWorldModel();
 }
