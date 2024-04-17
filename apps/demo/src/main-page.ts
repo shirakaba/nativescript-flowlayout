53e1d0efc22173ac4de25031f36adee95dc5fc93
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-04-17 22:00:06 +0900

    first visible text

--- apps/demo/src/main-page.ts
+++ apps/demo/src/main-page.ts
@@ -6,7 +6,11 @@ import type { EventData, Page } from "@nativescript/core";
 
 import { HelloWorldModel } from "./main-view-model";
 
+// CoreText came with macOS Cocoa; TextKit 1 and TextKit 2 came with iOS.
 // https://github.com/objcio/issue-5-textkit/tree/master/TextKitDemo
+// https://news.ycombinator.com/item?id=39603087
+// https://www.objc.io/issues/5-ios7/getting-to-know-textkit/
+// https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/TextLayout/TextLayout.html#//apple_ref/doc/uid/10000158i
 
 class Block {
   // One textStorage can hold multiple layoutManagers.
@@ -22,11 +26,65 @@ class Block {
   // e.g. to allow paginated layout (with each page containing a separate view).
   private readonly layoutManager = NSLayoutManager.new();
 
-  private readonly textContainer = NSTextContainer.new();
+  readonly textContainer = NSTextContainer.new();
 
   constructor() {
     this.textStorage.addLayoutManager(this.layoutManager);
     this.layoutManager.addTextContainer(this.textContainer);
+  }
+
+  private inlines = new Array<Inline>();
+
+  private addTextNodeOrInline(
+    childNode: TextNode | Inline,
+    attributes: NSDictionary<string, any>,
+  ) {
+    if (childNode instanceof TextNode) {
+      this.textStorage.appendAttributedString(
+        NSAttributedString.alloc().initWithStringAttributes(
+          childNode.text,
+          attributes,
+        ),
+      );
+      return;
+    }
+
+    for (const grandChildNode of childNode.getChildNodes()) {
+      this.addTextNodeOrInline(grandChildNode, attributes);
+    }
+  }
+
+  addInline(inline: Inline) {
+    for (const childNode of inline.getChildNodes()) {
+      this.addTextNodeOrInline(childNode, inline.attributes);
+    }
+
+    this.inlines.push(inline);
+  }
+}
+
+class TextNode {
+  constructor(public readonly text: string) {}
+}
+
+class Inline {
+  private _childNodes = new Array<TextNode | Inline>();
+
+  constructor(readonly attributes: NSDictionary<string, any>) {
+    //
+  }
+
+  *getChildNodes() {
+    for (const childNode of this._childNodes) {
+      yield childNode;
+    }
+  }
+
+  addTextNode(textNode: TextNode) {
+    this._childNodes.push(textNode);
+  }
+  addInline(inline: Inline) {
+    this._childNodes.push(inline);
   }
 }
 
@@ -36,14 +94,38 @@ export function navigatingTo(args: EventData) {
 
   const content = page.content;
   console.log(content);
-  console.log(new Block());
+  const block = new Block();
 
-  // @ts-ignore
-  console.log(JBBlock.new());
-  // @ts-ignore
-  console.log(JBTextStorage.new());
+  // I'm currently unclear whether attributes cascade from parent
+  // NSAttributedStrings to child ones.
+  for (let i = 0; i < 3; i++) {
+    // const dict = NSMutableDictionary.alloc().init();
+    // dict.setValueForKey(
+    //   i % 2 === 0 ? UIColor.brownColor : UIColor.blueColor,
+    //   NSForegroundColorAttributeName,
+    // );
 
-  // CoreText came with macOS Cocoa; TextKit 1 and TextKit 2 came with iOS.
-  // https://www.objc.io/issues/5-ios7/getting-to-know-textkit/
-  // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/TextLayout/TextLayout.html#//apple_ref/doc/uid/10000158i
+    const inline1 = new Inline({
+      [NSForegroundColorAttributeName]:
+        i % 2 === 0 ? UIColor.brownColor : UIColor.blueColor,
+    } as unknown as NSDictionary<string, any>);
+    inline1.addTextNode(
+      new TextNode(
+        "lorem ipsum dolor sit amet\nlorem ipsum dolor sit amet\nlorem ipsum dolor sit amet\n",
+      ),
+    );
+    block.addInline(inline1);
+  }
+
+  content.addEventListener("loaded", () => {
+    console.log("loaded!");
+    content.nativeView.addSubview(
+      UITextView.alloc().initWithFrameTextContainer(
+        CGRectMake(0, 0, 394, 760),
+        block.textContainer,
+      ),
+    );
+  });
+
+  // block.
 }
