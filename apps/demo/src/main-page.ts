7083d42e80017a7b29c940a2d0736b2c545f6f2c
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-05-26 14:31:48 +0900

    make Block responsible for managing all attribute strings

--- apps/demo/src/main-page.ts
+++ apps/demo/src/main-page.ts
@@ -6,7 +6,6 @@
 /// <reference types="@nativescript/types-ios" />
 
 import type { EventData, Page } from "@nativescript/core";
-import { dataSerialize } from "@nativescript/core/utils/native-helper.ios";
 import * as SymbolTree from "symbol-tree";
 import type * as TreeIterator from "symbol-tree/lib/TreeIterator";
 
@@ -28,13 +27,14 @@ const tree = new SymbolTree<BaseNode>("flow layout");
 // Currently TextNode extends this, which means it has to return `never` for
 // some of these hierarchy methods (it is a leaf node). Kinda shows we should
 // follow what DOM does instead (Node, Text, and Element). Let's refactor later.
-class BaseNode {
+abstract class BaseNode {
   get parent() {
     return tree.parent(this);
   }
   get children() {
     return tree.childrenIterator(this);
   }
+  abstract get textContent(): string;
 
   // Not yet sure about these two. We have addTextNode() and addInline(), but
   // we could replace them by overriding appendChild() and doing an instanceof
@@ -79,6 +79,14 @@ class Block extends BaseNode {
 
   readonly textContainer = NSTextContainer.new();
 
+  get textContent() {
+    let data = "";
+    for (const child of this.children) {
+      data += child.textContent;
+    }
+    return data;
+  }
+
   constructor() {
     super();
     this.textStorage.addLayoutManager(this.layoutManager);
@@ -114,6 +122,35 @@ class Block extends BaseNode {
     }
   }
 
+  /**
+   * Walks up the DOM parents to resolve the attributes to apply.
+   */
+  private static resolveAttributes(inline: Inline | Block) {
+    let attributes: Record<string, unknown> | undefined;
+
+    for (const ancestor of climbAncestors(inline) as Generator<
+      Inline | Block
+    >) {
+      if (!ancestor.attributes) {
+        continue;
+      }
+
+      for (const key in ancestor.attributes) {
+        // A child already has the attribute, so disregard the parent's value.
+        if (attributes?.[key]) {
+          continue;
+        }
+
+        if (!attributes) {
+          attributes = {};
+        }
+        attributes[key] = ancestor.attributes[key];
+      }
+    }
+
+    return attributes;
+  }
+
   addInline(inline: Inline) {
     // Need to set this from the start, as the TextNode grandchildren will be
     // climbing up to here during updateAttributes
@@ -121,14 +158,21 @@ class Block extends BaseNode {
 
     for (const childNode of inline.children) {
       if (childNode instanceof TextNode) {
-        // Update attributes (i.e. resolve the style cascade) before insertion
-        // into the native tree.
+        const attributes = Block.resolveAttributes(inline);
         console.log(
-          `[Block] updating attributes for "${childNode.data}".`,
-          dataSerialize(this.attributes),
+          `[Block] Appending inline "${childNode.data}"`,
+          attributes ?? "<no attributes>",
         );
-        childNode.updateAttributes();
-        this.textStorage.appendAttributedString(childNode.attributedString);
+        const placeholderString = NSAttributedString.alloc();
+
+        const attributedString = attributes
+          ? placeholderString.initWithStringAttributes(
+              childNode.data,
+              attributes as unknown as NSDictionary<string, unknown>,
+            )
+          : placeholderString.initWithString(childNode.data);
+
+        this.textStorage.appendAttributedString(attributedString);
         continue;
       }
 
@@ -141,13 +185,95 @@ class Block extends BaseNode {
   }
 
   /**
+   * Calculates the startOffset of the given descendant by walking in tree order
+   * counting all text leading up to it.
    *
-   * @param range The range into the textStorage affected by the update
-   * @param data The new data to write into that range
+   * @param descendant The descendant to count the text offset up until.
+   * @param traverseUntilAncestor Optional. Specifies the ancestor to stop
+   *   traversal at. Siblings preceding the ancestor, and ancestors of that
+   *   ancestor, will not contribute towards the startOffset. In other words,
+   *   the startOffset begins from this node.
+   *
+   *   Haven't yet decided how to handle nested blocks (is it possible to nest
+   *   NSTextStorage, or do we have to merge them, or is it impossible to
+   *   support altogether?). When the time comes, the consumer will have to
+   *   work out whether to stop traversal at the closest Block or walk the
+   *   whole tree.
+   * @returns
    */
-  onAncestorDidUpdateData(range: NSRange, data: string) {
-    // Ideally, the block should work out the range for us.
-    // https://github.com/jsdom/jsdom/blob/2f8a7302a43fff92f244d5f3426367a8eb2b8896/lib/jsdom/living/nodes/CharacterData-impl.js#L11
+  private static getStartOffsetOfDescendant(
+    descendant: BaseNode,
+    traverseUntilAncestor?: Block,
+  ) {
+    let startOffset = 0;
+    for (const ancestor of tree.ancestorsIterator(descendant)) {
+      if (ancestor === traverseUntilAncestor) {
+        break;
+      }
+
+      for (const prevSibling of tree.previousSiblingsIterator(ancestor)) {
+        startOffset += prevSibling.textContent.length;
+      }
+    }
+
+    return startOffset;
+  }
+
+  /**
+   * Descendants should call this method upon any data (text) change, so that
+   * this Block instance can update the text contents across the corresponding
+   * range.
+   *
+   * @param descendant The descendant TextNode that updated.
+   * @param prevData The previous data of that TextNode.
+   * @param newData The data that TextNode has just updated to.
+   */
+  onDescendantDidUpdateData(
+    descendant: TextNode,
+    prevData: string,
+    newData: string,
+  ) {
+    const startOffset = Block.getStartOffsetOfDescendant(descendant, this);
+
+    this.textStorage.replaceCharactersInRangeWithString(
+      {
+        location: startOffset,
+        length: prevData.length,
+      },
+      newData,
+    );
+  }
+
+  /**
+   * Descendants should call this method upon any attribute update, so that this
+   * Block instance can update the attributes across all affected ranges.
+   *
+   * @param descendant The descendant TextNode that updated.
+   * @param prevData The previous data of that TextNode.
+   * @param newData The data that TextNode has just updated to.
+   */
+  onDescendantDidUpdateAttributes(descendant: Inline) {
+    // Iterate over all descendants in tree order, updating attributes within
+    // the affected range.
+    //
+    // The search is inclusive, so begins with the descendant itself.
+    for (const node of tree.treeIterator(descendant)) {
+      if (!(node instanceof Inline) && !(node instanceof Block)) {
+        // Only act upon descendants that manage attributes.
+        continue;
+      }
+
+      const startOffset = Block.getStartOffsetOfDescendant(node, this);
+      const attributes = Block.resolveAttributes(node) ?? recycledEmptyObject;
+
+      this.textStorage.setAttributesRange(
+        attributes as unknown as NSDictionary<string, unknown>,
+        {
+          location: startOffset,
+          length: descendant.textContent.length,
+        },
+      );
+    }
   }
 }
 
@@ -166,6 +292,10 @@ class TextNode extends BaseNode {
   get data() {
     return this._data;
   }
+  get textContent() {
+    return this.data;
+  }
+
   /**
    * Replaces the original characters of the attributed string without clearing
    * attributes.
@@ -174,7 +304,8 @@ class TextNode extends BaseNode {
     const prevData = this._data;
     this._data = value;
 
-    this.parent?.onChildTextNodeDidUpdateData(this, prevData);
+    const closestBlock = closest(this, isBlock);
+    closestBlock?.onDescendantDidUpdateData(this, prevData, this._data);
   }
 
   appendData(data: string) {
@@ -189,73 +320,12 @@ interface Inline {
   removeChild(child: Inline | TextNode): void;
 }
 class Inline extends BaseNode {
-  // This is getting hard. Will try to port _referencedRanges and CharacterData
-  // from JSDOM instead.
-  //
-  // _referencedRanges: Set comes from Node:
-  // https://github.com/jsdom/jsdom/blob/main/lib/jsdom/living/nodes/Node-impl.js
-
-  onChildInlineDidUpdateData(child: Inline) {
-    this.parent?.onChildInlineDidUpdateData(this);
-  }
-  onChildTextNodeDidUpdateData(updatedChild: TextNode, previousData: string) {
-    const payload = Inline.getPayload({
-      updatedTextNode: updatedChild,
-      inline: this,
-      startOffset: 0,
-      previousData,
-    });
-
-    // this.parent?.onChildInlineDidUpdateData(this);
-
-    // TODO: convey payload to parent (which is complicated by it being
-    // Inline | Block).
-  }
-
-  private static getPayload({
-    updatedTextNode,
-    inline,
-    startOffset,
-    previousData,
-  }: {
-    updatedTextNode: TextNode;
-    inline: Inline;
-    startOffset: number;
-    previousData: string;
-  }) {
-    for (const childNode of inline.children) {
-      if (childNode instanceof TextNode) {
-        if (childNode === updatedTextNode) {
-          return {
-            startOffset,
-            previousData,
-            updatedData: childNode.data,
-          };
-        }
-
-        startOffset += childNode.data.length;
-        continue;
-      }
-
-      const payload = this.getPayload({
-        updatedTextNode,
-        inline: childNode,
-        startOffset,
-        previousData,
-      });
-      if (payload) {
-        return payload;
-      }
+  get textContent() {
+    let data = "";
+    for (const child of this.children) {
+      data += child.textContent;
     }
-
-    return null;
-  }
-
-  onChildInlineDidUpdateAttributes(_child: Inline) {
-    //
-  }
-  onChildInlineDidUpdateAttribute(_child: Inline) {
-    //
+    return data;
   }
 
   addTextNode(textNode: TextNode) {
@@ -278,35 +348,20 @@ class Inline extends BaseNode {
     }
     this.attributes[key] = value;
 
-    // Iterate over all inlines and cascade styles down to descendants (allowing
-    // clobbering by more specific styles).
-    for (const childNode of this.children) {
-      if (childNode instanceof TextNode) {
-        console.log(
-          `[Inline] updating attributes for "${childNode.data}".`,
-          dataSerialize(this.attributes),
-        );
-        childNode.updateAttributes();
-        continue;
-      }
-
-      childNode.setAttribute(key, value);
-    }
+    const closestBlock = closest(this, isBlock);
+    closestBlock?.onDescendantDidUpdateAttributes(this);
   }
   deleteAttribute(key: string) {
     if (!this.attributes) {
       return;
     }
     delete this.attributes[key];
-
-    for (const childNode of this.children) {
-      if (childNode instanceof TextNode) {
-        childNode.updateAttributes();
-        continue;
-      }
-
-      childNode.deleteAttribute(key);
+    if (!Object.keys(this.attributes).length) {
+      delete this.attributes;
     }
+
+    const closestBlock = closest(this, isBlock);
+    closestBlock?.onDescendantDidUpdateAttributes(this);
   }
 }
 
@@ -371,4 +426,32 @@ export function navigatingTo(args: EventData) {
     //   );
     // }, 1000);
   });
+}
+
+// TODO: try out tree.ancestorsIterator
+function* climbAncestors(node: BaseNode) {
+  let parent: BaseNode | null = node.parent;
+  while (parent) {
+    yield parent;
+    parent = parent.parent;
+  }
+}
+
+function closest<T extends BaseNode>(
+  self: BaseNode,
+  test:
+    | ((ancestor: unknown) => ancestor is T)
+    | ((ancestor: BaseNode) => boolean),
+) {
+  for (const ancestor of tree.ancestorsIterator(self)) {
+    if (test(ancestor)) {
+      return ancestor as T;
+    }
+  }
+
+  return null;
+}
+
+function isBlock(value: unknown): value is Block {
+  return value instanceof Block;
 }
