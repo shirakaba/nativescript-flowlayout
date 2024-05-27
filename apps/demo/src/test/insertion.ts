10cf9af22dbf90684658471976843edcfd70db04
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-05-28 00:19:38 +0900

    some tests on unstyled insertion

--- apps/demo/src/test/insertion.ts
+++ apps/demo/src/test/insertion.ts
@@ -17,12 +17,40 @@ test.before.each((context) => {
   context.setUp(block.textContainer);
 });
 
-test("Block > Inline > FlowText", ({ block }) => {
-  const inline = new Inline();
-  inline.appendChild(new FlowText("abc"));
-  inline.appendChild(new FlowText("def"));
-  inline.appendChild(new FlowText("ghi"));
-  block.appendChild(inline);
+// Unstyled tests
 
-  assert.is(block.debugDescription(), "abcdefghi");
+test("can append FlowTexts into Inlines", ({ block }) => {
+  // Adding an empty Inline should not change the Block's text content.
+  const inline = new Inline();
+  block.appendChild(inline);
+  assert.is(block.debugDescription(), "");
+
+  // Adding a FlowText into an already-added Inline should update the Block.
+  inline.appendChild(new FlowText("abc"));
+  assert.is(block.debugDescription(), "abc");
+
+  // Adding subsequent FlowTexts into a solitary Inline should update the Block.
+  inline.appendChild(new FlowText("def"));
+  assert.is(block.debugDescription(), "abcdef");
+});
+
+test("can nest Inlines", ({ block }) => {
+  // Adding an empty Inline should not change the Block's text content.
+  const parent = new Inline();
+  block.appendChild(parent);
+  assert.is(block.debugDescription(), "");
+
+  // Adding a nested empty Inline should not change the Block's text content.
+  const child = new Inline();
+  parent.appendChild(child);
+  assert.is(block.debugDescription(), "");
+
+  // Adding a FlowText into a nested Inline should update the Block.
+  child.appendChild(new FlowText("abc"));
+  assert.is(block.debugDescription(), "abc");
+
+  // Adding subsequent FlowTexts into a neighboured Inline should update the
+  // Block.
+  parent.appendChild(new FlowText("def"));
+  assert.is(block.debugDescription(), "abcdef");
 });
